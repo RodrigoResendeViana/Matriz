683059f003329d99a93078dd50c9855356fc60e0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-19 10:30:20 -0300

    matrizes

---
+++ ex2.py
@@ -0,0 +1,16 @@
+matriz = []
+
+for i in range(6):
+    lista = []
+    for j in range(6):
+        if i == j:
+            lista.append(1)
+        else:
+            lista.append(0)
+    matriz.append(lista)
+print(matriz)
+
+for i in range(len(matriz)):
+    for j in range (len(matriz[0])):
+        print(matriz[i][j], end='\t')
+    print()
